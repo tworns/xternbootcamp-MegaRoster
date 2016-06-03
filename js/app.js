8b617b7421b45898f783fcc9314593a62d6ff43c
tworns <tworns@yahoo.com>
2016-06-03 00:22:26 -0400

    Made a name required in the edit text field, made promote link convert to demote link after element was promoted

--- js/app.js
+++ js/app.js
@@ -36,6 +36,7 @@ var megaRoster =  {
   buildListItem : function(studentName) {
       var li = document.createElement('li');
       li.innerText = studentName;
+      var bordered = 0;
       var delLink = this.buildLink(
         { text: 'Remove',
         func: function(){
@@ -47,7 +48,16 @@ var megaRoster =  {
       var borderLink = this.buildLink(
         { text: 'Promote',
         func: function(){
+          if(bordered === 0) {
           li.style.border = '2px dashed blue';
+          bordered = 1;
+          this.text = 'Demote';
+        }
+        else{
+            li.style.border = 'none';
+            this.text = 'Promote';
+            bordered = 0;
+        }
         },
       } );
       li.appendChild(borderLink);
@@ -98,18 +108,23 @@ var megaRoster =  {
           var editBox = document.createElement('input');
           editBox.type = 'text';
           editBox.name = 'editName';
-          editBox.placeholder = 'New Name';
+          editBox.placeholder = studentName ;
+          editBox.required = true;
           editBox.id = 'edit';
           //
           var editConf = megaRoster.buildLink({
             text: 'Confirm',
             func: function(){
               var box = document.querySelector('#edit');
+              if(box.value !== "") {
               var newLi = megaRoster.buildListItem(box.value);
               li.parentElement.insertBefore(newLi,li);
               li.parentElement.removeChild(editLi);
               li.parentElement.removeChild(li);
-
+            }
+            else {
+              editBox.placeholder = "Please enter a value!";
+              }
             },
           });
           var editCancel = megaRoster.buildLink({
